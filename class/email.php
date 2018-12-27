795410c37fd4717bb38ab8a4d79b7334f686c102
valeriast <valeria.s@outlook.com.br>
2018-12-27 19:24:11 -0200

    Segunda opc index; base para newsletter

---
+++ class/email.php
@@ -0,0 +1,24 @@
+<?php
+
+class Email
+{
+
+  //Modelo, ajustar posteriormente
+
+  POST_email = 'email';
+  publlic $email;
+
+  public function post(){
+    $email=email;
+  }
+
+  public function get(){
+    echo($email);
+  }
+
+  public function gravaEmail(email, ){
+    //gravar em xml
+  }
+}
+
+?>

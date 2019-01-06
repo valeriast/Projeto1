f1df8ae4f7133dee24f3319cf777e11fdc3500c6
valeriast <valeria.s@outlook.com.br>
2019-01-06 20:15:13 -0200

    adicionado xml list

--- class/email.php
+++ class/email.php
@@ -7,5 +7,24 @@
 
   $fp = fopen('email.xml', 'a+');
   fwrite ($fp, $xml);
+
+  $xml = '<?xml version="1.0" encoding="ISO-8859-1"?>';
+  $xml .='<emails>';
+
+
+  $fp2 = fopen('lista.xml', 'w+');
+
+  $link = "lista.xml";
+  $xml = Simplexml_load_file($link) -> channel;
+  foreach ($xml -> email as $item) {
+        fwrite ($fp2,  $item -> email);
+  }
+  fclose ($fp2);
+
+  $fp2 = fopen('lista.xml', 'a+');
+  $xml ='</emails>';
+  fwrite ($fp2,  $xml);
+
+  fclose ($fp2);
   fclose ($fp);
 ?>
